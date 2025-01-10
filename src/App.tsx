66862f7fbce00999dc689fdd88ff1fb904c5ffe7
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-10 14:54:51 +0530

    typescript basics, making app type safe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Person } from "./Person";
+import { Person, Food } from "./Person";
 
 function App() {
   return (
     <div>
-      <Person name="sai" address="adslakdj" age={25} email="adsfadf" />
+      <Person
+        name="sai"
+        address="adslakdj"
+        age={25}
+        email="adsfadf"
+        friends={["sourav", "sai", "harami"]}
+        favFood={Food.momo}
+      />
     </div>
   );
 }
--- src/Person.tsx
+++ src/Person.tsx
@@ -3,6 +3,14 @@ interface Person {
   age: number;
   email: string;
   address: string;
+  friends: string[];
+  favFood: Food;
+}
+
+export enum Food {
+  pasta = "pasta",
+  sweets = "sweets",
+  momo = "momo",
 }
 
 export const Person = (props: Person) => {
@@ -12,6 +20,11 @@ export const Person = (props: Person) => {
       <h1>age: {props.age}</h1>
       <h1>email: {props.email}</h1>
       <h1>address: {props.address}</h1>
+      <h1>Friends:</h1>
+      {props.friends.map((ele) => {
+        return <h1>{ele}</h1>;
+      })}
+      <h1>Fav Food: {props.favFood}</h1>
     </div>
   );
 };
